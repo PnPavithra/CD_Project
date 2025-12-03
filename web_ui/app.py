c98cd84f3456a0517eea00e449f74e089dd0fd03
Rashmitha666 <rashmithamahesh666@gmail.com>
2025-12-03 22:37:17 +0530

    dont pull this code

--- web_ui/app.py
+++ web_ui/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import sys
 import os
+import json
 
 # Add parent directory to Python path so we can import gle_parser
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
@@ -12,11 +13,21 @@ app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def index():
     result = None
+    ast_data = "{}"
+
     if request.method == "POST":
         file = request.files["codefile"]
         code = file.read().decode("utf-8")
-        result = run_gle(code)
-    return render_template("index.html", result=result)
+
+        output = run_gle(code)
+
+        if isinstance(output, tuple) and len(output) == 2:
+            result, ast_data = output
+        else:
+            result = output
+
+    return render_template("index.html", result=result, ast=ast_data)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
